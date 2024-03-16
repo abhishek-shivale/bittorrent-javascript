58599754b364c5ad96940dd3b113080584ef4b11
CommitTODev <abhishekshivale45@gmail.com>
2024-03-16 10:30:14 +0530

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,32 +1,32 @@
 const process = require("process");
 const util = require("util");
 
-// Examples:
-// - decodeBencode("5:hello") -> "hello"
-// - decodeBencode("10:hello12345") -> "hello12345"
 function decodeBencode(bencodedValue) {
-  // Check if the first character is a digit
+  console.log(bencodedValue)
   if (!isNaN(bencodedValue[0])) {
     const firstColonIndex = bencodedValue.indexOf(":");
     if (firstColonIndex === -1) {
       throw new Error("Invalid encoded value");
     }
     return bencodedValue.substr(firstColonIndex + 1);
+  }else if (bencodedValue[0] === "i" && bencodedValue[bencodedValue.length - 1 ] === "e") {
+    return bencodedValue.substring(1, bencodedValue.length - 1 )
   } else {
     throw new Error("Only strings are supported at the moment");
   }
 }
+//&& 
 
 function main() {
   const command = process.argv[2];
 
   if (command === "decode") {
     const bencodedValue = process.argv[3];
+
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
   } else {
     throw new Error(`Unknown command ${command}`);
   }
 }
-
 
 main();
